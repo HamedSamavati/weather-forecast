394a3d222a26fa5dfacf33fe1554da5e8a684785
Hamed Samavati <hamedsamavaty@gmail.com>
2025-01-05 13:21:50 -0600

    adding config file

---
+++ config.js
@@ -0,0 +1,7 @@
+export const ENCRYPTED_APIKEY =
+  "U2FsdGVkX18JBk8dXH8d5I80FA4tni/VBEc9+hsJOUkuCyWrEt06eu4vLEmdyTGjerrQ53masteF0bGrXo9XWg==";
+
+export const ENCRYPTED_APIKEY_GEO =
+  "U2FsdGVkX1+o8G3V+DDE0FM2Ujbz4SEEyjuwxIiB6rfJ5Y+CpVkpFDa42EfCUsdmVkFe9xmTbo7sroTmmnC1Vw==";
+
+export const passphrase = "B@c0n&Ch33s3R@cks!2025#";
